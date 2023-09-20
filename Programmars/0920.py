4bb2a690c3fe750ea13007877dade78a1be65b4d
maxkim77 <141907655+maxkim77@users.noreply.github.com>
2023-09-20 19:45:58 +0900

    Update 0920.py

--- Programmars/0920.py
+++ Programmars/0920.py
@@ -15,6 +15,22 @@ def solution(str1, str2):
       result += str1[i] + str2[i]
     return result
 
+#문자 리스트를 문자열로 변환하기
+#문제 설명
+#문자들이 담겨있는 배열 arr가 주어집니다. arr의 원소들을 순서대로 이어 붙인 문자열을 return 하는 solution함수를 작성해 주세요.
+
+#제한사항
+#1 ≤ arr의 길이 ≤ 200
+#arr의 원소는 전부 알파벳 소문자로 이루어진 길이가 1인 문자열입니다.
+#입출력 예
+#arr	result
+#["a","b","c"]	"abc"
+
+def solution(arr):
+    return ''.join(arr)
+
+arr = ["a","b","c"]
+print(solution(arr))
 
 #문자열 곱하기
 
