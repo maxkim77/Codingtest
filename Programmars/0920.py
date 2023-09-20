9ad6df3038a588a986c19d12170cdf194bd73195
maxkim77 <141907655+maxkim77@users.noreply.github.com>
2023-09-20 19:58:40 +0900

    Update 0920.py

--- Programmars/0920.py
+++ Programmars/0920.py
@@ -15,6 +15,7 @@ def solution(str1, str2):
       result += str1[i] + str2[i]
     return result
 
+
 #문자 리스트를 문자열로 변환하기
 #문제 설명
 #문자들이 담겨있는 배열 arr가 주어집니다. arr의 원소들을 순서대로 이어 붙인 문자열을 return 하는 solution함수를 작성해 주세요.
@@ -24,28 +25,38 @@ def solution(str1, str2):
 #arr의 원소는 전부 알파벳 소문자로 이루어진 길이가 1인 문자열입니다.
 #입출력 예
 #arr	result
-#["a","b","c"]	"abc"
+#["a","b","c"]	#"abc"
 
 def solution(arr):
     return ''.join(arr)
 
-arr = ["a","b","c"]
+arr = ["a", "b", "c"]
 print(solution(arr))
 
 #문자열 곱하기
-
 #문제 설명
 #문자열 my_string과 정수 k가 주어질 때, my_string을 k번 반복한 문자열을 return 하는 solution 함수를 작성해 주세요.
 
-#제한사항
-#1 ≤ my_string의 길이 ≤ 100
-#my_string은 영소문자로만 이루어져 있습니다.
-#1 ≤ k ≤ 100
-#입출력 예
-#my_string	k	result
-#"string"	3	"stringstringstring"
-#"love"	10	"lovelovelovelovelovelovelovelovelovelove"
-
 def solution(my_string, k):
     answer = my_string * k
+    return answer
+    
+#문제 설명
+#연산 ⊕는 두 정수에 대한 연산으로 두 정수를 붙여서 쓴 값을 반환합니다. 예를 들면 다음과 같습니다.
+
+#12 ⊕ 3 = 123
+#3 ⊕ 12 = 312
+#양의 정수 a와 b가 주어졌을 때, a ⊕ b와 b ⊕ a 중 더 큰 값을 return 하는 solution 함수를 완성해 주세요.
+#단, a ⊕ b와 b ⊕ a가 같다면 a ⊕ b를 return 합니다.
+#제한사항
+#1 ≤ a, b < 10,000
+#입출력 예
+#a	b	result
+#9	91	991
+#89	8	898
+
+def solution(a, b):
+    concat_ab = int(str(a)+str(b))
+    concat_ba = int(str(b)+str(a))
+    answer = max(concat_ab, concat_ba)
     return answer
