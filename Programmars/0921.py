eae36d1f8c1d1db618e4555a2f94995da0327759
maxkim77 <141907655+maxkim77@users.noreply.github.com>
2023-09-21 20:41:44 +0900

    Update 0921.py

--- Programmars/0921.py
+++ Programmars/0921.py
@@ -19,3 +19,33 @@ def solution(n):
     else:
         answer = sum(i**2 for i in range(2,n+1,2))
     return answer
+#문제 설명
+#문자열에 따라 다음과 같이 두 수의 크기를 비교하려고 합니다.
+
+#두 수가 n과 m이라면
+#">", "=" : n >= m
+#"<", "=" : n <= m
+#">", "!" : n > m
+#"<", "!" : n < m
+#두 문자열 ineq와 eq가 주어집니다. ineq는 "<"와 ">"중 하나고, eq는 "="와 "!"중 하나입니다. 그리고 두 정수 n과 m이 주어질 때, n과 m이 ineq와 eq의 조건에 맞으면 1을 아니면 0을 return하도록 solution 함수를 완성해주세요.
+def solution(ineq, eq, n, m):
+    
+    answer = 0
+    
+    if ineq == ">" and eq == "=":
+        if n >= m:
+            answer = 1
+            
+    elif ineq == "<" and eq == "=":
+        if n <= m:
+            answer = 1
+            
+    elif ineq == ">" and eq == "!":
+        if n > m: 
+            answer = 0
+    
+    elif ineq == "<" and eq == "!":
+        if n < m: 
+            answer = 0
+    
+    return answer
