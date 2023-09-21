8589571d6207a8d67be38f640e3a0757859a9b09
maxkim77 <141907655+maxkim77@users.noreply.github.com>
2023-09-21 20:45:49 +0900

    Update 0921.py

--- Programmars/0921.py
+++ Programmars/0921.py
@@ -49,3 +49,17 @@ def solution(ineq, eq, n, m):
             answer = 0
     
     return answer
+#flag에 따라 다른 값 반환하기
+#문제 설명
+#두 정수 a, b와 boolean 변수 flag가 매개변수로 주어질 때, flag가 true면 a + b를 false면 a - b를 return 하는 solution 함수를 작성해 주세요.
+
+#제한사항
+#-1,000 ≤ a, b ≤ 1,000
+#입출력 예
+#a	b	flag	result
+#-4	7	true	3
+#-4	7	false	-11
+
+def solution(a, b, flag):
+    answer = a + b if flag else a - b
+    return answer
