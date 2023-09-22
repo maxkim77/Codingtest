6d9800cd45c9d96d48de7c9e6384bed232bbf3b2
maxkim77 <141907655+maxkim77@users.noreply.github.com>
2023-09-22 20:31:56 +0900

    Update 등차수열의 특정한 항만 더하기.py

--- 프로그래머스/unrated/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.py
+++ 프로그래머스/unrated/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.py
@@ -1,6 +1,6 @@
 def solution(a, d, included):
-    answer = 0
+    answer = 0  # 합을 저장할 변수 초기화
     for i, is_included in enumerate(included):
-        if is_included:
-            answer += a + i * d
-    return answer
+        if is_included:  # included 배열에서 true인 항만 더함
+            answer += a + i * d  # 등차수열의 일반항을 사용하여 항의 값을 계산하고 더함
+    return answer  # 합을 반환
