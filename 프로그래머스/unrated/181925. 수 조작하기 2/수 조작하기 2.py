6a614f0b7a153fb49d66c7b186c5bc25e27429f5
maxkim77 <141907655+maxkim77@users.noreply.github.com>
2023-09-23 21:01:37 +0900

    [unrated] Title: 수 조작하기 2, Time: 22.74 ms, Memory: 13.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181925. 수 조작하기 2/수 조작하기 2.py
@@ -0,0 +1,14 @@
+def solution(numLog):
+    result = []
+    for i in range(1, len(numLog)):
+        diff = numLog[i] - numLog[i-1]
+        if diff == 1:
+            result.append("w")
+        elif diff == -1:
+            result.append("s")
+        elif diff == 10:
+            result.append("d")
+        elif diff == -10:
+            result.append("a")
+    answer = ''.join(result)
+    return answer
