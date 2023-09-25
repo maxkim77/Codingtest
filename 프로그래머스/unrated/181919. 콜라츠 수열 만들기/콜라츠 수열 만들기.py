6b9d6f0a21788bdef130736e8e9ad24075bd81f4
maxkim77 <141907655+maxkim77@users.noreply.github.com>
2023-09-25 21:53:29 +0900

    [unrated] Title: 콜라츠 수열 만들기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.py
@@ -0,0 +1,10 @@
+def solution(n):
+    collatz = [n]
+    while n != 1:
+        if n % 2 == 0:
+            n = n // 2
+        else:
+            n = 3 * n + 1
+        collatz.append(n)
+    answer = collatz
+    return answer
