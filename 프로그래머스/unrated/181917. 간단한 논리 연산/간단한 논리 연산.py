bae5352b959c131170b056af9abdcd91422ecc34
maxkim77 <141907655+maxkim77@users.noreply.github.com>
2023-09-26 22:45:31 +0900

    [unrated] Title: 간단한 논리 연산, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181917. 간단한 논리 연산/간단한 논리 연산.py
+++ 프로그래머스/unrated/181917. 간단한 논리 연산/간단한 논리 연산.py
@@ -1,10 +1,7 @@
 def solution(x1, x2, x3, x4):
-    # 논리합(OR) 연산을 수행합니다.
     or_result1 = x1 or x2
     or_result2 = x3 or x4
     
-    # 논리곱(AND) 연산을 수행합니다.
     and_result = or_result1 and or_result2
     
-    # 최종 결과를 반환합니다.
     return and_result
