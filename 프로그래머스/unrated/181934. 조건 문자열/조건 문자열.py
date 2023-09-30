08fe1226f094c2120ae0e53f3312bfcc5cdf0c00
maxkim77 <141907655+maxkim77@users.noreply.github.com>
2023-09-30 12:00:13 +0900

    [unrated] Title: 조건 문자열, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181934. 조건 문자열/조건 문자열.py
@@ -0,0 +1,15 @@
+def solution(ineq, eq, n, m):
+    answer = 0
+    if ineq == ">" and eq == "=":
+        if n >= m:
+            answer = 1
+    if ineq == "<" and eq == "=":
+        if n <= m:
+            answer = 1
+    if ineq == ">" and eq == "!":
+        if n > m:
+            answer = 1
+    if ineq == "<" and eq == "!":
+        if n < m:
+            answer = 1  
+    return answer
