f1513b4cecedcca7e47034b7929c26a76d742e5a
maxkim77 <141907655+maxkim77@users.noreply.github.com>
2023-10-08 19:57:12 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
@@ -0,0 +1,10 @@
+def solution(num_list):
+    even_count = 0
+    odd_count = 0
+    
+    for num in num_list:
+        if num % 2 == 0:
+            even_count += 1
+        else:
+            odd_count += 1
+    return [even_count, odd_count]
