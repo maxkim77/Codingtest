6ab8e8d8e1a18eee1472842b501218354ecf2525
maxkim77 <141907655+maxkim77@users.noreply.github.com>
2023-11-03 21:14:51 +0900

    [level 2] Title: 주식가격, Time: 88.85 ms, Memory: 16.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -0,0 +1,9 @@
+def solution(prices):
+    n = len(prices)
+    answer = [0] * n
+    for i in range(n):
+        for j in range(i+1, n):
+            answer[i] += 1
+            if prices[i] > prices[j]:
+                break
+    return answer
