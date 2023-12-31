d3dd8ee3bcbb5ba4361bbebf8d3f36de230fd77a
maxkim77 <141907655+maxkim77@users.noreply.github.com>
2023-12-31 18:47:46 +0900

    [level unrated] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.01 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/unrated/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.py
+++ 프로그래머스/unrated/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.py
@@ -1,12 +1,28 @@
 def solution(bandage, health, attacks):
-    hp = health
-    start = 1
-    for i, j in attacks:
-        hp += ((i - start) // bandage[0]) * bandage[2] + (i - start) * bandage[1]
-        start = i + 1
-        if hp >= health:
-            hp = health
-        hp -= j
-        if hp <= 0:
-            return -1
-    return hp
+    t, x, y = bandage
+    current_health = health
+    continuous_success = 0
+    next_attack_index = 0
+
+    for second in range(1001):
+        if next_attack_index < len(attacks) and attacks[next_attack_index][0] == second:
+            current_health -= attacks[next_attack_index][1]
+            continuous_success = 0
+            next_attack_index += 1
+
+            if current_health <= 0:
+                return -1
+
+        else:
+            # 최대 체력을 초과하지 않는 경우에만 회복 적용
+            if current_health < health:
+                if continuous_success < t:
+                    current_health += x
+                    continuous_success += 1
+
+                # 추가 회복량 적용 후 다음 회복까지 기다림
+                if continuous_success == t:
+                    current_health = min(current_health + y, health)  # 추가 회복도 최대 체력을 초과하지 않도록
+                    continuous_success = 0  # 연속 성공 초기화
+
+    return current_health
