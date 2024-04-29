b6dbf0b22685b78351267d92409d3c9da997ff0a
maxkim77 <141907655+maxkim77@users.noreply.github.com>
2024-04-29 20:01:30 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.02 ms, Memory: 87.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        if (num1 == num2) // 조건문에서 괄호가 필요합니다.
+            return 1; 
+        else // else 다음에는 콜론(:)이 아니라 중괄호({})를 사용해야 합니다.
+            return -1;
+    }
+}
