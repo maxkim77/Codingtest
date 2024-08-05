3a3705a29488924d71b66b3baf4181a8f5f1e6eb
maxkim77 <141907655+maxkim77@users.noreply.github.com>
2024-08-05 14:34:51 +0900

    [level 0] Title: 순서 바꾸기, Time: 0.04 ms, Memory: 77 MB -BaekjoonHub

---
+++ 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        int[] answer = new int[num_list.length];
+        for (int i=0;i<n;i++){
+            answer[i+(num_list.length-n)] = num_list[i];
+        }
+        for (int i=n;i<num_list.length;i++){
+            answer[i-n] = num_list[i];
+        }
+        return answer;
+    }
+}
