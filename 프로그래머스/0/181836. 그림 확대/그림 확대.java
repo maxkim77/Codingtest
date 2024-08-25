f929ebdaa8dc7240168094936f1edc1a52742d50
maxkim77 <141907655+maxkim77@users.noreply.github.com>
2024-08-25 13:38:07 +0900

    [level 0] Title: 그림 확대, Time: 0.45 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181836. 그림 확대/그림 확대.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String[] solution(String[] picture, int k) {
+        String[] answer = new String[picture.length*k];
+        int index=0;
+        for(int i=0;i<picture.length;i++){
+            StringBuilder sb = new StringBuilder();
+            for(int j=0;j<picture[i].length();j++){
+                for(int n=0;n<k;n++){
+                    sb.append(picture[i].charAt(j));
+                }
+            }
+            for(int j=0;j<k;j++){
+                answer[index++] = sb.toString();
+            }
+        }
+        return answer;
+    }
+}
