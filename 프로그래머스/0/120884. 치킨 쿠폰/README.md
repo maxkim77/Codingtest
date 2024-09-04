5b10372d68a48f9474522cb7a703f4e6adfe80a4
maxkim77 <141907655+maxkim77@users.noreply.github.com>
2024-09-04 16:16:24 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.03 ms, Memory: 71.6 MB -BaekjoonHub

--- 프로그래머스/0/120884. 치킨 쿠폰/README.md
+++ 프로그래머스/0/120884. 치킨 쿠폰/README.md
@@ -1,6 +1,6 @@
 # [level 0] 치킨 쿠폰 - 120884 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120884) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120884?language=java) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 04일 16:15:26
+2024년 09월 04일 16:16:22
 
 ### 문제 설명
 
--- 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.java
+++ 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.java
@@ -1,16 +1,10 @@
 class Solution {
     public int solution(int chicken) {
         int answer = 0;
-        //처음 주문한 치킨만큼의 쿠폰 선언
-                //while 문 copon(10장이상일때)
-        // remain 값 계산
-        //  copon에 remain 값 넣기
-        int coupon = chicken;
-        while(coupon>=10){
-            int serviceChicken = coupon/10;
-            answer+=serviceChicken;
-            coupon = (coupon%10)+serviceChicken;
-        }
+        int service = chicken/10;
+        int sRemain = chicken%10;
+        int coupon = (sRemain + service)/10;
+        answer = service+ coupon;
         return answer;
     }
 }
