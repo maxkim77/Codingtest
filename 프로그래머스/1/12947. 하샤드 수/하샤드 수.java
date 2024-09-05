5b69694092c5a17e7325b07fdb0c067fb821f2af
maxkim77 <141907655+maxkim77@users.noreply.github.com>
2024-09-05 13:37:23 +0900

    [level 1] Title: 하샤드 수, Time: 0.03 ms, Memory: 85.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean solution(int x) {
+        boolean answer = true;
+        int sum=0;
+        int temp=x;
+        while(temp>0){
+            sum+=temp%10;
+            temp/=10;
+        }
+        if(x%sum!=0){
+            return false;
+        }
+        return answer;
+    }
+}
