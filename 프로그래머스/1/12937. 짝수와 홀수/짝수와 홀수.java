b1745c84cc1f378e139d69a2a8a2e4399f015995
maxkim77 <141907655+maxkim77@users.noreply.github.com>
2024-09-05 09:11:26 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.03 ms, Memory: 73.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String solution(int num) {
+        String answer = "";
+        if(num%2!=0){
+            answer="Odd";
+        }else if(num%2==0){
+            answer="Even";
+        }
+        return answer;
+    }
+}
