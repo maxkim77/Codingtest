6848b29c842968c1762c7f8996deac9f77cf603e
maxkim77 <141907655+maxkim77@users.noreply.github.com>
2024-09-13 12:13:38 +0900

    [level 1] Title: 소수 만들기, Time: 15.27 ms, Memory: 77.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        boolean chk = false;
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                for(int k=j+1;k<nums.length;k++){
+                    int num = nums[i] + nums[j] + nums[k];
+                    if(num>=2){
+                        chk = decimal(num);
+                    }
+                    if(chk==true){
+                        answer++;
+                    }
+                }
+                }
+            }
+            
+            return answer;
+        }
+
+
+    public boolean decimal(int num) {
+        boolean check = true;
+        if(num==2){
+            return check;
+        }
+        for(int i=2;i<num;i++){
+            if(num%i==0){
+                check = false;
+                break;
+            }
+        }
+        return check;
+    }
+}
