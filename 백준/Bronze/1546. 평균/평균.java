765671677c086bda96c21a4445e1641add14914c
maxkim77 <141907655+maxkim77@users.noreply.github.com>
2024-11-01 16:58:08 +0900

    [Bronze I] Title: 평균, Time: 252 ms, Memory: 19568 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int A[] = new int[N];
+
+        for(int i=0;i<N;i++){
+            A[i]=sc.nextInt();
+        }
+        long sum=0;
+        long max=0;
+        for(int i=0;i<N;i++){
+            if(max<A[i]){
+                max=A[i];
+            }
+            sum+= A[i];
+        }
+        System.out.println(sum*100.0/max/N);
+        sc.close();
+    }
+}
