bc72ef95fec2465e846ca938d4fa35ec4352fbb1
maxkim77 <141907655+maxkim77@users.noreply.github.com>
2025-09-04 21:04:00 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.02 ms, Memory: 80.7 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -3,12 +3,12 @@ import java.util.*;
 public class Solution {
     public int solution(int n) {
         int answer = 0;
-        int len = (int) Math.log10(n)+1;
-        for(int i=0;i<len;i++){
-            answer += n%10;
-            n=n/10;
+        
+        while (n > 0)
+        {
+           answer += n % 10;
+           n /= 10;
         }
-
         return answer;
     }
 }
