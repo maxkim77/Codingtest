657869b44b50ab83f38b27daf21d9fe9596a782f
maxkim77 <141907655+maxkim77@users.noreply.github.com>
2026-04-12 12:34:39 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 86.24 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -3,21 +3,23 @@ class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
         HashMap<String, Integer> map = new HashMap<>();
-    for (String p : participant)
-    {
-        map.put(p, map.getOrDefault(p, 0) + 1);
-    }
-    for (String c : completion)
-    {
-        map.put(c, map.get(c) - 1);   
-    }
-    for (String key : map.keySet()){
-        if (map.get(key) > 0)
+        
+        for (String p : participant)
         {
-            answer = key;
-            break;
+          map.put(p, map.getOrDefault(p, 0) + 1);   
         }
-    }
+        for (String c : completion)
+        {
+            map.put(c, map.get(c) - 1);
+        }
+        for (String key: map.keySet())
+        {
+            if(map.get(key) > 0)
+            {
+                answer = key;
+                break;
+            }
+        }
         return answer;
     }
 }
