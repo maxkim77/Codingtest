37295c421b38669480f0db73dfb192223411b5b7
maxkim77 <141907655+maxkim77@users.noreply.github.com>
2026-06-07 20:35:50 +0900

    [level 2] Title: 의상, Time: 0.16 ms, Memory: 32.6 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -1,6 +1,16 @@
+import java.util.*;
+
 class Solution {
     public int solution(String[][] clothes) {
-        int answer = 0;
-        return answer;
+        int answer = 1;
+        HashMap<String, Integer> map = new HashMap<>();
+        for (String[] cloth : clothes) {
+            map.put(cloth[1], map.getOrDefault(cloth[1], 0) + 1);
+        }
+        for (int cnt : map.values()) {
+            answer *= (cnt + 1);
+        }
+        
+        return answer - 1;
     }
 }
